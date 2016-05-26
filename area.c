9a7d8bdbaecc6895e95d73d36c4c571fb20e182d
rajeshwarisalunkhe <rajeshwarisalunkhe@gmail.com>
2016-05-26 12:45:58 +0530

    updated

--- area.c
+++ area.c
@@ -1,4 +1,15 @@
-/* 19 
+/* 19 The area of a triangle can be computed by the sine law when 2
+sides of the triangle and the angle between them are known.
+Area = (1 / 2 ) ab sin ( angle )
+Given the following 6 triangular pieces of land, write a
+program to find their area and determine which is largest,
+Plot No. a b angle
+1 137.4 80.9 0.78
+2 155.2 92.62 0.89
+3 149.3 97.93 1.35
+4 160.0 100.25 9.00
+5 155.6 68.95 1.25
+6 149.7 120.0 1.75 
 
 */
 
